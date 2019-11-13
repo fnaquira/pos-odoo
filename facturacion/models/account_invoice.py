f5201df3bca4b5a1dd81964933455ac0c7fa308b
Favio Náquira <fnaquira@conflux-aqp.com>
2019-11-12 22:47:27 -0500

    se elimina __pycache__

--- facturacion/models/account_invoice.py
+++ facturacion/models/account_invoice.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
-class Series(models.Model):
+class AccountInvoice(models.Model):
 	_inherit = 'account.invoice'
 	
-	serie_id = fields.Many2one(comodel_name='facturacion.series', string='Serie Electrónica')
+	serie_id = fields.Many2one(comodel_name='facturacion.series',
+		string='Serie Electrónica')
